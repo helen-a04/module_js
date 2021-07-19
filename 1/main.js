a7f9f094ef5018ebbb5e3f4dc39d93e75bb3e70d
helen_a04 <ak.helena09@gmail.com>
2021-07-19 20:01:59 +0300

    lesson1

---
+++ 1/main.js
@@ -0,0 +1,49 @@
+//var msg = 'It is good!';
+//var num = 15;
+//console.log(typeof(num));
+
+//var a = 10;
+//var b = 7;
+ //var c = a + b;
+
+ // console.log(-c);  //унарный минус - меняет положительное значение на отрицательное
+ 
+ //var remains = a % b;
+ //console.log(remains);
+
+// var stepen = a ** b;
+// console.log(stepen);
+
+ //a++;
+ //b--;
+ //console.log(a, b);
+
+ //a *= 3; //то же самое что и a = a * 3
+ //b += a;
+ //console.log(a, b);
+
+ //КОНКАТЕНАЦИЯ - сложение слов 
+
+ //var msg = 'Hello';
+ //var name = ' Helen';
+ //var msg2 = ', how are you?';
+
+ //var result = msg + name + msg2;
+
+ //console.log(result);
+
+ //console.log(50 > 30);
+ //console.log(50 < 30);
+
+ //console.log(50 >= 50);
+
+ //console.log(50 == 50);
+ //console.log(50 != 50);
+
+ //console.log(50 === '50');
+
+ //var numStr = ' 123 ';
+ //console.log(Number(numStr));
+
+var numStr = 123;
+ console.log(Boolean(numStr));
